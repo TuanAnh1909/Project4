2c97a10650459f50957a5ab1bd8da4ef52c712b1
Tuan Anh <tuananhnb1999@gmail.com>
2023-09-10 00:52:32 +0700

    push

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api-dev.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://udagram-api-dev.eba-rhp3yfsg.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
